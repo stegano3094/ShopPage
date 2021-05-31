7468e780144a9a856a1f6391afb172fd68c3bd90
stegano <steganowork@naver.com>
2021-06-01 01:21:16 +0900

    webDesign

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Navbar, Container, Nav, NavDropdown, Jumbotron, Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <button type="button" class="btn btn-info">Info</button>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Jumbotron className="background">
+        <h1>Welcome~</h1>
+        <p>
+          This is a simple hero unit.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
+            <h4>상품명</h4>
+            <p>상품 설명 & 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"/>
+            <h4>상품명</h4>
+            <p>상품 설명 & 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"/>
+            <h4>상품명</h4>
+            <p>상품 설명 & 가격</p>
+            </div>
+        </div>
+      </div>
+
     </div>
   );
 }
